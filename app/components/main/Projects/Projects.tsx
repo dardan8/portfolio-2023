51d18da6d53e613f42627e80360e88171f7685d5
dardan8 <dardan.dara@gmail.com>
2023-05-26 01:00:36 +0200

    Styling

--- app/components/main/Projects/Projects.tsx
+++ app/components/main/Projects/Projects.tsx
@@ -5,12 +5,14 @@ import ProjectCard from "../../global/ProjectCard/ProjectCard";
 const Projects = () => {
   return (
     <div className={styles.projects}>
-      <ProjectCard project='/images/projects/pomodoro.png' />
-      <ProjectCard project='/images/projects/Blog_Image.png' />
-      <ProjectCard project='/images/projects/pomodoro.png' />
-      <ProjectCard project='/images/projects/pomodoro.png' />
-      <ProjectCard project='/images/projects/pomodoro.png' />
-      <ProjectCard project='/images/projects/pomodoro.png' />
+      <div className={styles.projectspace}>
+        <ProjectCard project='/images/projects/pomodoro.png' />
+        <ProjectCard project='/images/projects/Blog_Image.png' />
+        <ProjectCard project='/images/projects/pomodoro.png' />
+        <ProjectCard project='/images/projects/pomodoro.png' />
+        <ProjectCard project='/images/projects/pomodoro.png' />
+        <ProjectCard project='/images/projects/pomodoro.png' />
+      </div>
     </div>
   );
 };
